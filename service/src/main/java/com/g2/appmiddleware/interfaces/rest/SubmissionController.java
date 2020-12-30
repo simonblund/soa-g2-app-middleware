3d74769a49553aee2cc64bffe1f4d6e64efbb9e8
Kai Weeks <kai.weeks@gmail.com>
2020-12-30 19:27:38 +0000

    Log4js output from controller for Submission endpoints

--- service/src/main/java/com/g2/appmiddleware/interfaces/rest/SubmissionController.java
+++ service/src/main/java/com/g2/appmiddleware/interfaces/rest/SubmissionController.java
@@ -6,24 +6,27 @@ import com.g2.appmiddleware.api.rest.submission.SubmissionResponse;
 import com.g2.appmiddleware.api.rest.submission.SubmissionVerificationRequest;
 import com.g2.appmiddleware.application.SubmissionService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class SubmissionController {
     private final SubmissionService service;
 
     @CrossOrigin(origins = "*")
     @GetMapping(UrlPaths.EXAMINATION)
     public ResponseEntity<SubmissionCollectionResponse> getSubmissionsForExamination(@PathVariable String examinationCode) {
+        log.info("Endpoint {} hit with GET for {}", UrlPaths.EXAMINATION, examinationCode);
         return ResponseEntity.ok(service.getSubmissionsForExam(examinationCode));
     }
 
     @CrossOrigin(origins = "*")
     @PostMapping(UrlPaths.SUBMISSION_VERIFY)
     public ResponseEntity<Void> verify(SubmissionVerificationRequest request){
-
+        log.info("Endpoint {} hit with POST for {}", UrlPaths.SUBMISSION_VERIFY, request.getSubmissionId());
         try {
             service.verify(request.getSubmissionId());
             return ResponseEntity.ok(null);
