b2295c8e4d1ba7772faa39437ffa04b791f68c10
Simon Blomsterlund <simon@simonblund.com>
2021-01-10 18:35:31 +0200

    Added bookings to the api

--- service/src/main/java/com/g2/appmiddleware/interfaces/rest/ScheduleController.java
+++ service/src/main/java/com/g2/appmiddleware/interfaces/rest/ScheduleController.java
@@ -2,11 +2,20 @@ package com.g2.appmiddleware.interfaces.rest;
 
 import com.g2.appmiddleware.api.rest.UrlPaths;
 import com.g2.appmiddleware.infrastructure.rest.ScheduleServiceClient;
+import com.g2.scheduleservice.api.rest.resource.BookingResponse;
+import com.g2.scheduleservice.api.rest.resource.ResourceResponse;
+import com.g2.scheduleservice.api.rest.resource.RoomResponse;
 import com.g2.scheduleservice.api.rest.schedule.CourseOccasionScheduleResponse;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Description;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.g2.appmiddleware.api.rest.UrlPaths.*;
 
 @Slf4j
 @RestController
@@ -29,5 +38,36 @@ public class ScheduleController {
         log.info("A POST request to {} has arrived with courseOccasionId {}", UrlPaths.GET_COURSE_INSTANCE, courseOccasionId);
         return scheduleServiceClient.saveToCanvas(courseOccasionId, canvasToken, canvasUser, request);
 
+    }
+
+    
+    @CrossOrigin(origins = "*")
+    @GetMapping(BOOKING_RESOURCE)
+    ResponseEntity<List<BookingResponse>> getAllBookings(){
+        return scheduleServiceClient.getAllBookings();
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(GET_FROM_RESOURCE_ID)
+    ResponseEntity<BookingResponse> bookResource(@PathVariable long resourceId, @RequestBody BookingResponse request){
+        return scheduleServiceClient.bookResource(resourceId,request);
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(GET_FROM_ROOM_ID)
+    ResponseEntity<BookingResponse> bookRoom(@PathVariable long roomId, @RequestBody BookingResponse request){
+        return scheduleServiceClient.bookRoom(roomId, request);
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping(BOOKING_RESOURCE_RESOURCE)
+    ResponseEntity<List<ResourceResponse>> getAllResources(){
+        return scheduleServiceClient.getAllResources();
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping(BOOKING_ROOM_RESOURCE)
+    ResponseEntity<List<RoomResponse>> getAllRooms(){
+        return scheduleServiceClient.getAllRooms();
     }
 }
